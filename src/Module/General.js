5aa1016859a936e7877057223844b8375b509007
ZachaRuba <zacharyjcanderson@gmail.com>
2022-03-29 18:55:13 -0700

    Updated with some team info

--- src/Module/General.js
+++ src/Module/General.js
@@ -23,6 +23,8 @@ import moon from "./Assets/teami/moon.png";
 import Ryah from "./Assets/teami/Ryah.jpg";
 import lyin from "./Assets/teami/lyin.jpg";
 import zoha from "./Assets/teami/zoha.png";
+import Zach from "./Assets/teami/Zach.jpg";
+import John from "./Assets/teami/John.jpg";
 
 /** Put all your Team member image inside folder `Team Images`
  * and import them like in the above and update src in TeamInfo
@@ -36,32 +38,30 @@ import boy from "./Assets/boy.png";
 
 
 const TOP_SECTION = {
-  TITLE: "Join Limbo Hacks",
-  Typed_effect: ["24 hours of creation", "Win awesome prizes"],
+  TITLE: "Join OrcaOverdrive's Robotics Team",
+  Typed_effect: ["Gain Technical Skills and Experience", "Learn about teamwork and leadership"],
   SHORT_DESCRIPTION:
-    "Join us on 20th November 2021 with over 300 students from across the nation for 24 hours of creation, innovation, & fun.",
+    "Join us for creation, innovation, & fun.",
   IMG_SRC: boy,
   DISCORD_LINK: "",
-  JUDGES_FORM_LINK:
-    "https://forms.office.com/Pages/ResponsePage.aspx?id=DQSIkWdsW0yxEjajBLZtrQAAAAAAAAAAAAN__jZdNhdUNVRQMUg4VllMRURDN1lON0haNFVCQUgySC4u",
   HACKERS_REGISTRATION_FORM_LINK:
-    "https://forms.office.com/Pages/ResponsePage.aspx?id=DQSIkWdsW0yxEjajBLZtrQAAAAAAAAAAAAN__jZdNhdUNTgyREk0VzdEMU9RVFlPMVNWVEJQUkNYMi4u"
+    "mailto:recruiting@orcaoverdrive.com"
 };
 
 const SOCIALS = {
-  instagram: "https://www.instagram.com/limbohacks/",
-  discord: "https://discord.com/invite/8XJSzmtWPp",
-  linkedin: "https://www.linkedin.com/company/limbo-hacks/",
-  twitter: "https://twitter.com/HacksLimbo",
-  devpost: "https://limbo-hacks-12968.devpost.com/",
-  email: "mailto:hello@limbohacks.tech",
-  mail: "hello@limbohacks.tech"
+  instagram: "",
+  discord: "",
+  linkedin: "",
+  twitter: "",
+  devpost: "",
+  email: "mailto:info@orcaoverdrive.com",
+  mail: "info@orcaoverdrive.com"
 };
 
 const MIDDLE_SECTION = {
-  TITLE: "What is Limbo hacks?",
+  TITLE: "What we do.",
   LONG_DESCRIPTION:
-    "Hack Limbo is 24 hour long running hackathon will be held on November 20th & 21th , We calls for hackers, artists, coders, designers, tech evangelists, creatives, and developers from all disciplines to take a part to win awesome prizes and collaborate with other developers . Throughout the weekend hackers at Limbo Hacks will have unique opportunities to learn from each other trying hands in new technologies We are determined to have an all-inclusive and diverse group of students attending.",
+    "Orca Overdrive is an open team for highschool students to join who are passionate about STEM and Robotics.  As a team, we tackle difficult engineering problems and challenges to build our skills and practice gracious professionalism.  We are based in north van but students from all regions are welcome!",
   LOGO_EFFECT: true,
   LOGO: ""
 };
@@ -73,7 +73,7 @@ const FOOTER = {
   },
   JOIN_TEAM: {
     required: true,
-    src: "https://forms.office.com/Pages/ResponsePage.aspx?id=DQSIkWdsW0yxEjajBLZtrQAAAAAAAAAAAAN__jZdNhdUOUhEQkJSRk02OEU4SlBMTUdYOFIxOTNZSy4u"
+    src: "mailto:recruiting@orcaoverdrive.com"
   },
   Privacy_policy: {
     required: true,
@@ -102,53 +102,10 @@ const Prizeinfo = [
     //Array 1
     {
       icon: <i class="first fas fa-4x fa-trophy"></i>,
-      type: "overall First",
+      type: "FIRST Paci : Quarter Semi-Finalists",
       content:
-        "First Overall prize will be given to a project that outstands all other submissions"
-    },
-    {
-      icon: <i class=" second fas fa-4x fa-medal"></i>,
-      type: "overall Second",
-      content:
-        "Second Overall prize will be given to the second best project of the hackathon"
-    },
-    {
-      icon: <i class=" third fas fa-4x fa-skiing-nordic"></i>,
-      type: "Third Second",
-      content:
-        "Best third overall project of the hackathon will win some awesome prizes"
-    }
-  ],
-  [
-    //Array 2
-    {
-      icon: <i class="fab fourth fa-3x fa-wpbeginner"></i>,
-      type: "Best Solo",
-      content:
-        "You project will qualify for this prize if you participate alone without a team though we encourage you to participate with a team"
-    },
-    {
-      icon: <i class="first fas fa-4x fa-trophy"></i>,
-      type: " Best Beginner",
-      content: "Your project will qualify for this category if atleast 50% "
-    },
-    {
-      icon: <i class=" fifth fa-3x fas fa-book-open"></i>,
-      type: "Best UI/UX",
-      content: "Project with most creative designs will be UI/UX track"
-    }
-  ],
-  [
-    //Array 3
-    {
-      icon: <i class="fas fa-4x sixth fa-male"></i>,
-      type: "Best Web App with Qoom",
-      content: "Must use qoom in your project to win this category"
-    },
-    {
-      icon: <i class="fas fa-user-friends seventh fa-3x "></i>,
-      type: "More prizes",
-      content: "More prizes will be revealed later"
+        "Competed in Canadian Pacific North Western FRC Competition in 2022 and made it to the Quater Semi-Finals.",
+      src: "https://youtube.com/playlist?list=PLZT9pIgNOV6Zm41-OUqrR1KSHAmyMul1w"
     }
   ]
 ];
@@ -163,22 +120,22 @@ const TeamInfo = [
   [
     //Array 1
     {
-      Name: "Rehan",
-      role: "Organizer",
-      github: "https://github.com/Limbo-Hacks/Hackathon-website-template",
+      Name: "Vince",
+      role: "Team Captain",
+      github: "",
       linkedin: "",
       img: me
     },
     {
-      Name: "Moon",
-      role: "Organizer",
+      Name: "Hoshyar",
+      role: "Member",
       github: "",
       linkedin: "",
       img: moon
     },
     {
-      Name: "Erika",
-      role: "Organizer",
+      Name: "Eemaan",
+      role: "Member",
       github: "",
       linkedin: "",
       img: Ryah
@@ -187,19 +144,26 @@ const TeamInfo = [
   [
     //Array 2
     {
-      Name: "Lyanola",
-      role: "Organizer",
+      Name: "Connor",
+      role: "Member",
       github: "",
       linkedin: "",
       img: lyin
     },
     {
-      Name: "Zoheb",
-      role: "Organizer",
+      Name: "Andy",
+      role: "Member",
       github: "",
       linkedin: "",
       img: zoha
-    },    
+    },
+    {
+      Name: "Daylen",
+      role: "Member",
+      github: "",
+      linkedin: "",
+      img: zoha
+    }  
   ] 
 ];
 
@@ -207,22 +171,22 @@ const JudgesInfo = [
   [
     //Array 1
     {
-      Name: "Rehan",
-      role: "Organizer",
-      github: "",
-      linkedin: "",
-      img: me
+      Name: "Zachary Anderson",
+      role: "Lead Mentor",
+      github: "https://github.com/ZachaRuba",
+      linkedin: "https://www.linkedin.com/in/zachary-anderson-434084167/",
+      img: Zach
     },
     {
-      Name: "Moon",
-      role: "Organizer",
+      Name: "John Omeljaniuk",
+      role: "Mentor",
       github: "",
       linkedin: "",
-      img: moon
+      img: John
     },
     {
-      Name: "Ryah Garcia",
-      role: "Organizer",
+      Name: "Robert Olson",
+      role: "Mentor",
       github: "",
       linkedin: "",
       img: Ryah
@@ -231,19 +195,12 @@ const JudgesInfo = [
   [
     //Array 2
     {
-      Name: "Lyanola",
-      role: "Organizer",
+      Name: "Ross Janke",
+      role: "Mentor",
       github: "",
       linkedin: "",
       img: lyin
-    },
-    {
-      Name: "Zoheb",
-      role: "Organizer",
-      github: "",
-      linkedin: "",
-      img: zoha
-    },    
+    }  
   ] 
 ];
 
@@ -268,40 +225,41 @@ const frequentlyAskedQuestions  = [
   [
     [
       {
-        label: "What is a hackathon?",
+        label: "What do you do on the team?",
         content:
-        'A hackathon is a 24 hour event where "hackers" all over the globe will collaborate on creating a functioning software and hardware by the end of the event.'      },
-      {
-        label: "Who can attend?",
-        content: "Limbo Hacks is open to all undergraduate, high school students and middle school students from all schools."
+        'Our team is constantly registering in robotics competitions around the world to compete in.  We have competed in the FIRST Robotics Competition and MATE ROV in the past.'      
       },
       {
-        label: "I am a first time hacker, what should I do?",
-        content: "No worries, we love to welcome first time hackers and start their hacking journey from a hackathon. We have some awesome workshops and events which will help you get started with hackathon and hacking in general."
+        label: "Who can join?",
+        content: "Anyone in High School Grades 8-12."
       },
       {
-        label: "How team formation works?",
-        content: "You can form team with 4 members in it at maximum. If you don't have a team, there'll be plenty of opportunities for you to meet hackers and form a team!"
+        label: "Can I join if I don't have any experience?",
+        content: "No worries, we love to welcome first timers and start their journey into robotics. You will be working with team captains and sponsors that you can learn from."
+      },
+      {
+        label: "I'm not in high school.  Can I still help?",
+        content: "This team is only for high school students however, if you are a university student or graduated engineer we are always looking for more mentors!"
       }
     ],
     [
       {
-        label: "How to register myself in the hackathon?",
-        content: "All you need is to fill our form above and join Discord, we will guide you through everything there"
+        label: "How do I join?",
+        content: "Email us!  You or your parents will have to sign some liability and photo/video waivers when you join."
       },
       {
         label: "I have more questions?",
-        content: "Reach us directly at (rehan@limbohacks.tech or hello@limbohacks.tech) we would happy to help you."
+        content: "Reach us directly at (info@orcaoverdrive.com) we would happy to help you."
       }
       ,
       {
-        label: "Can i volunteer?",
-        content: "Yes we are actively looking for volunteers, fill the form (in footer section) we will be glad to have you."
+        label: "When do you meet?",
+        content: "Every saturday from 9:30am to 12:30pm we have a general group meeting at Zen Maker Lab's HQ 272 E 1st Street in North Vancouver BC."
       }
       ,
       {
         label: "Will there be swag?",
-        content: "Yes! Participants will recieveing Limbo Hacks swag!."
+        content: "We make T-shirts and Stickers for each event with our sponsors!."
       }
     ]
   ],
